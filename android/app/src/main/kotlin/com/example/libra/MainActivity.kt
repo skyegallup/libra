a8fda035edf214d0ec4786563738d3692d5a2e51
Skylar Gallup <skyegallup@outlook.com>
2025-11-15 17:24:49 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/libra/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.libra
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/widgets.dart';
+import 'package:libra/views/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Add MixTheme
+    return WidgetsApp(
+      title: 'Libra',
+      home: const HomePage(),
+      color: Color.fromRGBO(0, 255, 0, 1.0),
+      pageRouteBuilder: <T>(RouteSettings settings, WidgetBuilder builder) => PageRouteBuilder<T>(
+        settings: settings,
+        pageBuilder: (BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation) => builder(context)
+      )
+    );
+  }
+}
